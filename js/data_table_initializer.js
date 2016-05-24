043133d351abaedef8df93b6e28ab1b9989645b4
Cristian Muscalu <muscaiu@yahoo.com>
2016-05-24 18:10:24 +0300

    trying to make sum of time

--- js/data_table_initializer.js
+++ js/data_table_initializer.js
@@ -1,5 +1,5 @@
 // //Usage
-$(document).ready(function() {
+$(document).ready(function() {   
 	var table = $('#myTable').DataTable({
 				stateSave: true,
 				iDisplayLength: 10,
@@ -24,83 +24,74 @@ $(document).ready(function() {
                             footer: true
                         }    
                     ], 
-                oSelectorOpts: { filter: 'applied', order: 'current'} ,	
-                //columnDefs: [
-                    //     //column 6 set to "not searchable"" 
-                    //     { 
-                            
-                    //         "searchable": true, targets: [0,1,2,3,4,5] }
-                    // ],
+                oSelectorOpts: { filter: 'applied', order: 'current'} ,
 
 // <START> Code for Footer SUM/////////////
                 footerCallback: function ( row, data, start, end, display ) {
-                    var tempo_new = this.api(), data;
-                    var quantita = this.api(), data;
-        
                     // Remove the formatting to get integer data for summation
                     var intVal = function ( i ) {
-                        return typeof i === 'string' ?
-                            i.replace(/[\$,]/g, '')*1 :
-                            typeof i === 'number' ?
-                                i : 0;
-                                };    
-                    // Total tempo_new over all pages
-                    total_tempo_new = tempo_new
-                        .column( 3 )
-                        .data()
-                        .reduce( function (a, b) {
-                            return intVal(a) + intVal(b);
-                        }, 0 ); 
-                    // Total tempo_new over this page
-                    pageTotal_tempo_new = tempo_new
-                        .column( 3, { page: 'current'} )
-                        .data()
-                        .reduce( function (a, b) {
-                            return intVal(a) + intVal(b);
-                        }, 0 ); 
+                        return typeof i === 'string' ? i.replace(/[\$,]/g, '')*1 :
+                               typeof i === 'number' ? i : 0;
+                         };                                                                      
+   
+                    // var tempo_new = this.api(), data;
+                    // // Total tempo_new over all pages
+                    // total_tempo_new = tempo_new 
+                    //     .column( 3 )
+                    //     .data()
+                    //     .reduce( function (a, b) {
+                    //         return intVal(a) + intVal(b);
+                    //     }, 0 ); 
                         
-                    //Solution 1 - OK
-                    //Moment.js, Moment.duration.js for hh:mm:ss transformation
-                    //For Current Page
-                    var time_min = total_tempo_new;
-                    time_min =  parseInt(time_min) + (time_min * 60 % 60) / 60;
-                    var Total_format_time = moment.duration(time_min ,'minutes').format("HH:mm:ss", {trim:false});     
-                    //For Total Pages
-                    var page_time_min = pageTotal_tempo_new;
-                    page_time_min =  parseInt(page_time_min)+ (page_time_min * 60 % 60) / 60;
-                    var pageTotal_format_time = moment.duration(page_time_min ,'minutes').format("HH:mm:ss", {trim:false}); 
-                    
-                    //Solution 2 - OK
-                    // var value = total_tempo_new;
-                    // var minutes = Math.floor(value)
-                    // var seconds = (value - minutes) * 100;
-                    // var duration = moment.duration(minutes, 'minutes').add(seconds, 'seconds');
-                    // var format_time = moment.utc(duration.asMilliseconds()).format("HH:mm:ss");
-                    
-                    // Update footer Column "tempo_new"
-                    $( tempo_new.column( 3 ).footer() ).html(
-                        pageTotal_format_time +' ('+ Total_format_time +' total)'
-                    );
-                    
-                    // Total quantita over all pages
+                    // // Total tempo_new over this page
+                    // pageTotal_tempo_new = tempo_new
+                    //     .column( 3, { page: 'current'} )
+                    //     .data()
+                    //     .reduce( function (a, b) {
+                    //         return intVal(a) + intVal(b);
+                    //     }, 0 ); 
+                        
+                    // //Solution 1 - OK
+                    // //Moment.js, Moment.duration.js for hh:mm:ss transformation
+                    // //For Current Page
+                    // var time_min = total_tempo_new;
+                    // time_min =  parseInt(time_min) + (time_min * 60 % 60) / 60;
+                    // console.log(time_min);
+                    // var Total_format_time = moment.duration(time_min ,'minutes').
+                    //                         format("HH:mm:ss", {trim:false});
+                    // //For Total Pages
+                    // var page_time_min = pageTotal_tempo_new;
+                    // page_time_min =  parseInt(page_time_min)+ (page_time_min * 60 % 60) / 60;
+                    // var pageTotal_format_time = moment.duration(page_time_min ,'minutes').
+                    //                             format("HH:mm:ss", {trim:false}); 
+                    // // // Update footer Column "tempo_new"
+                    // $( tempo_new.column( 3 ).footer() ).html(
+                    //     pageTotal_format_time +' ('+ Total_format_time +' total)'
+                    // );                                                                 
+
+                    var quantita = this.api(), data;
                     total_quantita = quantita
                         .column( 5 )
                         .data()
                         .reduce( function (a, b) {
                             return intVal(a) + intVal(b);
                         }, 0 ); 
-                    // Total quantita over this page
+
                     pageTotal_quantita = quantita
                         .column( 5, { page: 'current'} )
                         .data()
                         .reduce( function (a, b) {
                             return intVal(a) + intVal(b);
-                        }, 0 ); 
-                    // Update footer Column "quantita"
+                        }, 0 );                        
+
                     $( quantita.column( 5 ).footer() ).html(
                     pageTotal_quantita +' ('+ total_quantita +' total)'
                     );
-                                
+                    
+                    var tempo_api = this.api();
+                    var total = tempo_api.column(3).data().sum();
+                    var pageTotal = tempo_api.column(3, {page:'current'}).data().sum();
+                    $(tempo_api.column(3).footer()).html(pageTotal + ' (' + total + ' total)');                                    
                 }
                 });	
 // </END> Code for Footer SUM/////////////
@@ -110,14 +101,8 @@ $(document).ready(function() {
         table.draw(); } );
     $('#to').change( function() { 
         table.draw(); } );
-    $('#myTable_length').change( function() { 
-        table.draw(); } );     
-    //  $('#MySearch').on('keyup', function(){
-    //     table
-    //         .columns( [0, 1, 2, 3, 4] )
-    //         .search(this.value)
-    //         .draw();
-    // })
+    $('#myTable_length').change( function() {
+        table.draw(); } );       
     
     //Enable Search ONLY for column 0,1,2,3,4,5 (only option found!) 
     //By overwriting the search method
@@ -197,3 +182,54 @@ $.fn.dataTableExt.afnFiltering.push(
 	}
 );
 
+
+/********************** SUM PLUGIN ***********************
+ * Fairly simply, this plug-in will take the data from an API result set
+ * and sum it, returning the summed value. The data can come from any data
+ * source, including column data, cells or rows.
+ *
+ * Note that it will attempt to 'deformat' any string based data that is passed
+ * into it - i.e. it will strip any non-numeric characters in order to make a
+ * best effort attempt to sum all data types. This can be useful when working
+ * with formatting numbers such as currency. However the trade-off is that no
+ * error is thrown if non-numeric data is passed in. You should be aware of this
+ * in case unexpected values are returned - likely the input data is not what is
+ * expected.
+ *
+ *  @name sum()
+ *  @summary Sum the values in a data set.
+ *  @author [Allan Jardine](http://sprymedia.co.uk)
+ *  @requires DataTables 1.10+
+ *
+ *  @returns {Number} Summed value
+ *
+ *  @example
+ *    // Simply get the sum of a column
+ *    var table = $('#example').DataTable();
+ *    table.column( 3 ).data().sum();
+ *
+ *  @example
+ *    // Insert the sum of a column into the columns footer, for the visible
+ *    // data on each draw
+ *    $('#example').DataTable( {
+ *      drawCallback: function () {
+ *        var api = this.api();
+ *        $( api.table().footer() ).html(
+ *          api.column( 4, {page:'current'} ).data().sum()
+ *        );
+ *      }
+ *    } );
+ */
+
+jQuery.fn.dataTable.Api.register( 'sum()', function ( ) {
+	return this.flatten().reduce( function ( a, b ) {
+		if ( typeof a === 'string' ) {
+			a = a.replace(/[^\d.-]/g, '') * 1;
+		}
+		if ( typeof b === 'string' ) {
+			b = b.replace(/[^\d.-]/g, '') * 1;
+		}
+
+		return a + b;
+	}, 0 );
+} );
