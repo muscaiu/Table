412b31230e6bdfabc97cb7bc57b62fbd59feeafc
Cristian Muscalu <muscaiu@yahoo.com>
2016-06-01 15:45:37 +0300

    found another solution for data column disable search but not working

--- js/data_table_initializer.js
+++ js/data_table_initializer.js
@@ -17,8 +17,9 @@ $(document).ready(function () {
                     return header[column]; //header is the array I used to store header texts
                 },
                 body: function (data, column, row){
-                    return column === 5 ?
-                    data.replace( /[qwe,]/g, '' ) :
+                    return column === 4 ?
+                    //data.replace( /[test]/, 'REPLACED' ) :
+                    data.replace( 'test', 'REPLACED' ) :
                     data;
                 }
             }
@@ -32,12 +33,11 @@ $(document).ready(function () {
         pagingType: "full_numbers",
         lengthMenu: [[10, 50, 100, -1], [10, 50, 100, "All"]],
         dom: 'lfBrtip',
-
         buttons: [
-            /************** EXPORT CSV **************/
+            /************** EXPORT CSV ALL **************/
             $.extend(true, {}, buttonExp, {
                 extend: 'csvHtml5',
-                text: 'Csv All',
+                text: 'CSV ALL',
                 exportOptions: {
                     modifier: {
                         page: 'current'
@@ -45,25 +45,40 @@ $(document).ready(function () {
                 },                
                 footer: true
             }),
-            /************** EXPORT XLS **************/
+            /************** EXPORT CSV Selected **************/
+            $.extend(true, {}, buttonExp, {
+                extend: 'csvHtml5',
+                text: 'CSV Selected',
+                exportOptions: {                    
+                    modifier: {
+                        page: 'current',
+                        selected: true
+                    }
+                },
+                footer : true
+            }),
+            
+            /************** EXPORT XLS ALL **************/
             $.extend(true, {}, buttonExp, {
                 extend: 'excelHtml5',
-                text: 'Xls All',
+                text: 'XLS ALL',
                 exportOptions: {
+                    //orthogonal: 'export',
                     modifier: {
                         page: 'current'
                     }
                 },
-                footer: true
+                footer: true,
                 // customize: function ( xslx ) {
                 //     var sheet = xlsx.xl.worksheets['sheet1.xml']; 
                 //     $('c[r=A1] t', sheet).text( 'Custom text' );
                 // }
             }),
+            /************** EXPORT XLS Selected **************/
                 $.extend(true, {}, buttonExp, {
                 extend: 'excelHtml5',
-                text: 'Xls Selected',
-                exportOptions: {
+                text: 'XLS Selected',
+                exportOptions: {                    
                     modifier: {
                         page: 'current',
                         selected: true
@@ -72,10 +87,10 @@ $(document).ready(function () {
                 footer : true
             }),
 
-            $.extend(true, {}, buttonExp, {
-               extend: 'pdfHtml5',
-               footer: true
-            }),
+            // $.extend(true, {}, buttonExp, {
+            //    extend: 'pdfHtml5',
+            //    footer: true
+            // }),
         ],
         oSelectorOpts: { filter: 'applied', order: 'current' },
 
@@ -148,7 +163,6 @@ $(document).ready(function () {
                 // add 0 to minute and second if single digit , slice(-2) will select last 2 digit
                 return m1 + ':' + ('0' + m2).slice(-2) + ':' + ('0' + m3).slice(-2); // return updated string
             })
-            //Number(tempoPage, tempoTotal);
             //write in footer
             $(tempo.column(3)
                 .footer())
@@ -199,12 +213,14 @@ $(document).ready(function () {
         // table.buttons(0,null).containers().appendTo('#containerId');
         //}
         initComplete: function () {
-            var api = this.api();
-            this.api().columns().eq(0).each(function (index) {
-                if (index == 1 || index == 4) {
+            //var api = this.api();
+            //this.api().columns().eq(0).each(function (index) {
+            this.api().columns().every( function () {
+                //if (index == 1 || index == 4) {
                     var column = this;
                     var select = $('<select class="form-control"><option value=""></option></select>')
-                        .appendTo($(api.column(index).header()))
+                        //.appendTo($(api.column(index).header()))
+                        .appendTo( $(column.header()))
                         .on('change', function () {
                             var val = $.fn.dataTable.util.escapeRegex(
                                 $(this).val()
@@ -212,21 +228,21 @@ $(document).ready(function () {
 
                             column
                                 //.row(index).data()
-                                //.search(val ? '^' + val + '$' : '', true, false)
-                                .search( val ? val : '', true, false )
+                                //.search( val ? val : '', true, false )
+                                .search(val ? '^' + val + '$' : '', true, false)                                
                                 .draw();
                         });
                     var i = 0;
-                    api.column(index).data().unique().sort().each(function (d, j) {
-                        select.append('<option value="' + d + '">' + d + '</option>');
+                    //api.column(index).data().unique().sort().each(function (d, j) {
+                        column.data().unique().sort().each( function ( d, j ) {
+                        select.append( '<option value="'+d+'">'+d+'</option>' )
                     });
-                }
+                //}
             });
         }
     });
 
     /************ <END> Footer SUM ****************/
-
     // Redraw Table on inputs click
     $('#from').change(function () {
         table.draw();
@@ -259,17 +275,9 @@ $(document).ready(function () {
         $('select', this).click(function (event) {
             event.stopPropagation();
         });
-    });
+    });   
 
-    //Add the excel button to dom
-    new $.fn.dataTable.Buttons(table, {
-        "buttons": [$.extend(true, {}, buttonExp, {
-            extend: 'excelHtml5'
-        })]
-    });
-    
-    $("select").on("change",function(){table.row($(this).parents("tr")).data()});
-    });
+});
 
 
 //Date Range Picker
--- session.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = @mysql_connect("5.2.255.236","root","bitech");
-if (!$con){
-	die("Can't connect: " . mysql_error());
-}
-mysql_select_db("db_automa",$con);
-$sql = "SELECT * FROM lavorazioni";
-$myData = mysql_query($sql, $con);
-?>  
